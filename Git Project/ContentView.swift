6ce1008e84e883feff4ff762bb43b5edcdb6a723
Princeton Ace Lopez <princetonacelopez@icloud.com>
2020-09-29 13:18:43 +0800

    Change color to purple

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 destination: ModalView()) {
                 Text("Go Next")
                
-        }
+            }.foregroundColor(.purple)
         .navigationBarTitle("Xcode and Git")
         }
     }
