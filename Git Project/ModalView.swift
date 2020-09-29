e15163fe99eb65e239095df333bc5188f93c913d
Princeton Ace Lopez <princetonacelopez@icloud.com>
2020-09-29 13:01:48 +0800

    Created separated modal view

---
+++ Git Project/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  Git Project
+//
+//  Created by Princeton Ace Lopez on 9/29/20.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal view")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
