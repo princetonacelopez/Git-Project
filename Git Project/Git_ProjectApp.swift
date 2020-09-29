bcf627f280f0f7508b141e1b7c6a2ba1eeecc2d6
Princeton Ace Lopez <princetonacelopez@icloud.com>
2020-09-29 12:53:08 +0800

    Initial Commit

---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Princeton Ace Lopez on 9/29/20.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
